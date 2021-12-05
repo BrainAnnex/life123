bcb276916fafa76c6fd2a8141e6d07dcea1e186c
Julian West <julian@BrainAnnex.org>
2021-12-05 01:51:21 -0800

    early setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # life1D
 Modeling of pseudo-1D biological systems
+
+(Upcoming website: https://life1D.org)
---
+++ bio_sim.py
@@ -0,0 +1,10 @@
+class BioSim:
+
+    def initialize_universe(self, n_cells: int, n_species: int):
+        """
+
+        :param n_cells:
+        :param n_species:
+        :return:
+        """
+        pass
