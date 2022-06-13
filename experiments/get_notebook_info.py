3665b26eaf74c50425f369d73e889c24514f20d1
Julian West <julian@BrainAnnex.org>
2022-06-13 13:54:53 -0700

    Renamed ipynbname to ipynbname3.  Introduced set_path to more notebooks

--- experiments/get_notebook_info.py
+++ experiments/get_notebook_info.py
@@ -1,12 +1,12 @@
 """
-IMPORTANT: meant *only* for Jupyterlab 3 notebooks.
+IMPORTANT: meant *only* for JupyterLab 3 notebooks.
 
 Return the name - WITHOUT extension - of the JupyterLab notebook invoking this function.
 This version is based on a modified version of the library "ipynbname",
 and appears to work on a variety of systems.
 """
 
-from experiments.ipynbname import name
+from experiments.ipynbname3 import name
 
 
 
--- experiments/life_1D/diffusion/set_path.py
+++ experiments/life_1D/diffusion/set_path.py
@@ -1,24 +1,40 @@
 """
 A copy of this file should be present in every folder that is not at the project's root level.
 
-Only needed in notebooks that weren't started with a script adding the project root to sys.path
-(but no harm if used in those as well.)
+Only needed when JupyterLab isn't started with an option that adds the project root to sys.path
+(as done, for example, in the "quick.bat" script at the top level.)
+There's no harm in using this module under all circumstances.
 
-*** IMPORTANT: change this relative path as needed in different folder!  (In the line below
-that sets PROJECT_HOME_DIR
 
-To use, simply insert the following line at the top of your notebooks:
+TO USE:
+Insert the following two lines at the top of your notebooks:
+
 import set_path
+set_path.add_ancestor_dir_to_syspath(3)     # IMPORTANT: change this number as needed
 """
 
 import sys
 import pathlib
 
 
-def add_home_dir_to_syspath():
-    PROJECT_HOME_DIR = pathlib.Path.cwd().parent.parent.parent   # IMPORTANT: change this relative path as needed!
-    sys.path.append(str(PROJECT_HOME_DIR))
-    print(f"Added '{PROJECT_HOME_DIR}' to sys.path")
- 
+def add_ancestor_dir_to_syspath(n: int, verbose=True) -> None:
+    """
+    Add the n-th ancestor of the current working directory to the system path
 
-add_home_dir_to_syspath()
+    :param n:
+    :param verbose:
+    :return:        None
+    """
+    directory = pathlib.Path.cwd()      # Start with the current working directory
+
+    for _ in range(n):
+        directory = directory.parent    # Navigate up one level in the file hierarchy
+
+    #PROJECT_HOME_DIR = pathlib.Path.cwd().parent.parent.parent   # IMPORTANT: change this relative path as needed!
+    sys.path.append(str(directory))
+
+    if verbose:
+        print(f"Added '{directory}' to sys.path")
+
+
+#add_ancestor_dir_to_syspath(3)
