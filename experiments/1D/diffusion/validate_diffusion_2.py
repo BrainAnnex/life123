2e52d79a5ee5bfdd366048a046a6932c54dc4e78
Julian West <californialife88@gmail.com>
2022-10-05 23:43:09 -0700

    Revise experiment "1D/diffusion/validate_diffusion_1"
    Start an initial draft of experiment "1D/diffusion/validate_diffusion_2"

--- experiments/1D/diffusion/validate_diffusion_2.py
+++ experiments/1D/diffusion/validate_diffusion_2.py
@@ -16,8 +16,13 @@
 # ## Carry out a few single indivual steps of diffusion, 
 # ### and directly verify that the values satisfy the diffusion equation
 #
-# In this "PART 1", we'll be looking at a tiny system with just 10 bins, to easily inspect the numbers directly.
-# We'll use concentrations initialized to an upward-shifted sine wave with 1 cycle over the length system
+# In this "PART 2", we quickly repeat all the steps discussed at length in part1,
+# but using a much-finer resolution.
+# This time, we'll use a slightly more complex initial concentrations built from 2 sine waves.
+#
+# **We'll also explore the effects of:**  
+# -Spacial resolution ("delta x")  
+# -Temporal resolution ("delta t")
 #
 # LAST REVISED: Oct. 5, 2022
 
@@ -46,13 +51,16 @@ diffusion_rate = 10.
 
 chem_data = chem(diffusion_rates=[diffusion_rate], names=["A"])
 
+# %% [markdown]
+# ### Prelude : start with a tiny system and directly inspect the numbers
+# #### For this small system, we'll initialize the concentrations to a sine wave with 1 cycle over the system
+# (with a bias to always keep it > 0)
+
 # %%
 # Initialize the system with just a few bins
 bio = BioSim1D(n_bins=10, chem_data=chem_data)
 
 # %%
-# Initialize the concentrations to a sine wave with 1 cycle over the system
-# (with a bias to always keep it > 0)
 bio.inject_sine_conc(species_name="A", frequency=1, amplitude=10, bias=50)
 
 # %%
@@ -78,11 +86,10 @@ fig.data[0].ygap=1
 fig.show()
 
 # %% [markdown]
-# ### Now do 4 rounds of single-step diffusion, to collect the system state at a total of 5 time points: 
-# #### t0 (the initial state), plus t1, t2, t3 and t4
+# #### Now do 4 rounds of single-step diffusion, to collect the system state at a total of 5 time points: t0 (the initial state), plus t1, t2, t3 and t4
 
 # %%
-# All the system states will get collected in this object
+# All the system state will get collected in this object
 history = MovieArray()
 
 # %%
@@ -97,8 +104,8 @@ history.get_array()
 # %%
 # Additional parameters of the simulation run (the diffusion rate got set earlier)
 delta_t = 0.01
-delta_x = 2       # Note that the the number of bins also define the fraction of the 2Pi cycle in each bin
-algorithm = None  # "Explicit, with 3+1 stencil"
+delta_x = 2
+algorithm = None
 
 # %%
 # Do the 4 rounds of single-step diffusion; show the system state after each step, and accumulate all data
@@ -117,33 +124,30 @@ all_history
 
 # %% [markdown]
 # #### Each row in the above matrix is a state snapshot at a different time:
-# first row is the initial state at time 0; the successive rows are at t1, t2, t3, t4
+# first row is the initial state at time 0; the successive rows are t1, t2, t3, t4
 
 # %%
-# Let's consider the state, i.e. the concentration across the bins, at the midpoint in time (t2)
+# Let's consider the state at the midpoint in time (t2)
 f_at_t2 = all_history[2]
 f_at_t2
 
 # %% [markdown]
-# If one compares the above state (at t2) with the initial state (the top row in the matrix), 
+# If one compare the above state (at t2) with the initial state (the top row in the matrix), 
 # one can see, for example, that the leftmost bin's concentration is increasing ("pulled up" by its neighbor to the right):
 # 50. has now become 50.28881576
 #
-# The rightmost bin's concentration is decreasing ("pulled down" by its neighbor to the left):
+# The rightmost's bin's concentration is decreasing ("pulled down" by its neighbor to the left):
 # 44.12214748 has now become 43.94505274
 
 # %% [markdown]
-# ## The diffusion equation states that the partial derivative of the concentration values with respect to time  must equal the (diffusion rate) times (the 2nd partial derivative  with respect to space).
+# ### The diffusion equation states that the partial derivative of the concentration values with respect to time  must equal the (diffusion rate) times the 2nd partial derivative  with respect to space .
 # Let's see if that is the case for the values at time t2!  We are picking t2 because we need at least a value at the earlier time, and a value at the later time
-
-# %% [markdown]
-# ## A. The 2nd partial derivative  with respect to space
 
 # %%
 # A simple-minded way of computing the 2nd spacial derivative is to use the Numpy gradient function TWICE across the x value
 # (that function approximates the derivative using differences)
-gradient_x_at_t2 = np.gradient(f_at_t2, delta_x)   # Start with taking the first derivative
-gradient_x_at_t2    # This will be the partial derivative of the concentration with respect to x, at time t2
+gradient_x = np.gradient(f_at_t2, delta_x)
+gradient_x
 
 # %% [markdown]
 # For example, the 2nd entry in the above array of estimated derivatives, can be manually checked from the 1st and 3rd value in the function f_at_t2(x), as follows:
@@ -155,69 +159,61 @@ gradient_x_at_t2    # This will be the partial derivative of the concentration w
 # #### Now take the derivative again, with the Numpy gradient function, to arrive at a coarse estimate of the 2nd derivative with respect to x:
 
 # %%
-second_gradient_x_at_t2 = np.gradient(gradient_x_at_t2, delta_x)
-second_gradient_x_at_t2
+second_gradient_x = np.gradient(gradient_x, delta_x)
+second_gradient_x
 
 # %% [markdown]
 # Note how the 2nd derivative is 0 at bin 5 (bins are numbered 0 thru 9): if you look at the earlier sine plot, x5 is the inflection point.
 
 # %% [markdown]
-# ### B. The partial derivative with respect to time
-# Now, let's look at how concentrations change with time.  Let's first revisit the full history:
+# ### Now, let's look at how concentrations change with time.  Let's first revisit the full history:
 
 # %%
 all_history
 
 # %% [markdown]
-# #### For simplicity, let's start by just inspecting how the values change over time at the *3rd bin* from the left, 
+# #### For simplicity, let's start by just inspecting how the values change over time at the 3rd bin from the left, 
 # i.e. the 3rd *column* (index 2 because counting starts at 0) of the above matrix:
 
 # %%
-f_of_t_at_x2 = all_history[ : , 2]  # The column with index 2
+f_of_t_at_x2 = all_history[ : , 2]
 f_of_t_at_x2
 
 # %% [markdown]
-# ### The above a function of time 
-# (we took an entry from each row - the rows being the system states at t0, t1, t2, t3, t4); let's look at its time derivative:
+# ### This a function of time: let's look at its time derivative:
 
 # %%
-gradient_t_at_x2 = np.gradient(f_of_t_at_x2, delta_t)
-gradient_t_at_x2
+gradient_t = np.gradient(f_of_t_at_x2, delta_t)
+gradient_t
 
 # %% [markdown]
 # ### The above is the rate of change of the concentration, as the diffusion proceeds, at the position x2
 # At time t2, the midpoint in the simulation, the value is:
 
 # %%
-gradient_t_at_x2[2]
+gradient_t[2]
 
 # %% [markdown]
-# ### C. All said and done, we have collected the time derivative and the 2nd spacial derivative, at the point (x2, t2).
-# Do those values satisfy the diffusion equation??  Does the time derivative indeed equal the (diffusion rate) x (the 
-# 2nd spacial derivative)?  Let's see:
+# ### All said and done, we have collected the time derivative and the 2nd spacial derivative, at the point (x2, t2).
+# Do those values satisfy the diffusion equation??  Does the 2nd spacial derivative indeed equal the diffusion rate times the time derivative?  Let's see:
 
 # %%
-gradient_t_at_x2[2]
+gradient_t[2]
 
 # %%
-diffusion_rate * second_gradient_x_at_t2[2]
+diffusion_rate * second_gradient_x[2]
 
 # %% [markdown]
-# ## D. The 2 value indeed roughly match - considering the coarseness of the large spacial grid, and the coarseness of estimating the derivatives numerically. 
-# We have evidence that the diffusion equation may indeed be satisfied by the values we obtained from our diffusion algorithm, in the proximity of the point (x2, t2)
+# ## The 2 value indeed roughly match - considering the coarseness of the large spacial grid, and the coarseness of estimating the derivatives numerically
 
 # %% [markdown]
-# ### E. Finally, instead of just scrutining the match at the point x2, let's do that for all the points in space at time t2,
-# WITH THE EXCEPTION of the outmost points (because the numeric estimation of the derivatives gets very crummy at the boundary).
-#  
-# Let's first re-visit all the data once again:
+# #### Finally, instead of just scrutining the match at the point x2, let's do that for all the points in space at time t2, WITH THE EXCEPTION of the outmost points (because the numeric estimation of the derivatives gets very crummy at the boundary).  Let's first re-visit all the data once again:
 
 # %%
 all_history
 
 # %%
-# The following is just an expanded version of what we did before for the time derivative; 
-# instead of just considering 1 column,
+# This is just an expanded version of what we did before; instead of just consider a column,
 # like we did before, we're now repeating the computations along all columns
 # (the computations are applied vertically, "along axis 0" in Numpy-speak)
 gradient_t = np.apply_along_axis(np.gradient, 0, all_history, delta_t)
@@ -229,17 +225,14 @@ gradient_t_at_t2 = gradient_t[2]
 gradient_t_at_t2
 
 # %% [markdown]
-# Note the value -8.94751865, 3rd from left : that's the single value we looked at before (at point x2)
-
-# %% [markdown]
-# #### Time to again check the match of the two sides of the diffusion equation
+# Note the value -8.94751865, 3rd from left : that's the single value we looked at before
 
 # %%
-lhs = gradient_t_at_t2   # The LEFT-hand side of the diffusion equation, as a vector for all the spacial points at time t2
+lhs = gradient_t_at_t2   # The left-hand side of the diffusion equation, as a vector for all the spacial points at time t2
 lhs
 
 # %%
-rhs = diffusion_rate * second_gradient_x_at_t2  # The RIGHT-hand side of the diffusion equation, again as a vector
+rhs = diffusion_rate * second_gradient_x  # Same data dimension as above, but for the RIGHT-hand side of the diffusion equation
 rhs
 
 # %% [markdown]
@@ -249,13 +242,12 @@ rhs
 lhs - rhs
 
 # %%
-# Here we usea handy function to compare two equal-sized vectors, 
-# while optiong to disregarding a specified number of entries at each edge.
+# A handy function to compare two equal-sized vector, disregarding a specified number of entries at each edge
 # It returns the Euclidian distance ("L2 norm") of the shortened vectors
 num.compare_vectors(lhs, rhs, trim_edges=1)
 
 # %% [markdown]
-# #### IMPORTANT: all values in this experiment are VERY coarse, because of the large effective delta_x (the tiny number of bins.)
-# In part2 (notebook "validate_diffusion_2"), much-better approximations will get looked at!
+# #### IMPORTANT: all values are very coarse, to utilize tiny arrays that are easy to visually inspect.
+# In part2 of the experiment ("validate_diffusion_2"), much-better approximations will get looked at
 
 # %%
