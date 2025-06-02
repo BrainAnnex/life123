25668a6dc82a030ab2aec3cc9eab199e244fb9e5
Julian West <41893046+BrainAnnex@users.noreply.github.com>
2025-06-01 21:28:31 -0700

    Change of version number to "1.0.0rc5"

--- life123/__init__.py
+++ life123/__init__.py
@@ -10,7 +10,7 @@
     :license:       MIT.  See LICENSE file for more details.
 """
 
-__version__ = "1.0.0rc4"
+__version__ = "1.0.0rc5"     # IN-PROGRESS
 
 
 from life123.bio_sim_1d import BioSim1D
